d45b18bfda6f61448daf9baa30bbd85f6afbfce7
simone <s1763f@gmail.com>
2022-06-06 05:07:59 +0200

    Update main.c

--- main.c
+++ main.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include "utils.h"
+#include <stdlib.h>
+#include "util.h"
+#include "queue.h"
 
 int main(int argc, char* argv[])
 {
-
+  argc = 2;
   FILE* fd;
-  int n;
-  int quanto;
-  char i;
-  float wait = 0;
+  unsigned n;
+  unsigned quanto;
 
   fd = fopen(argv[1], "r");
   if (fd == NULL) {
@@ -18,113 +17,66 @@ int main(int argc, char* argv[])
     exit(1);
   }
 
-  fscanf(fd, "%c %d %d", &i, &n, &quanto);
+  fscanf(fd, "# %d %d\n", &n, &quanto);
+  Queue_t* queue = initQueue(n);
+  proc_t* v = malloc(n * sizeof(proc_t));
 
-  process* v_mod = malloc(n * sizeof(process));
-  process* v = malloc(n * sizeof(process));
-
-  for (int i = 0; i < n; i++) {
-    fscanf(fd, "%s %d %d", &v[i].nome, &v[i].burst_cpu, &v[i].priority);
-  }
-
-  for (int i = 0; i < n; i++) {
-    printf("%s %d %d\n", v[i].nome, v[i].burst_cpu, v[i].priority);
-  }
-
-  // Una copia immodificata del vettore per il Round Robin
-  for (int i = 0; i < n; i++) {
-    strcpy(v_mod[i].nome, v[i].nome);
-    v_mod[i].burst_cpu = v[i].burst_cpu;
-    v_mod[i].priority = v[i].priority;
+  for (unsigned i = 0; i < n; i++) {
+    proc_t* proc = (proc_t*)malloc(sizeof(proc_t));
+    fscanf(fd, "%s %ld %ld", v[i].nome, &v[i].burst_cpu, &v[i].priority);
+    proc->burst_cpu = v[i].burst_cpu;
+    proc->perm_burst = proc->burst_cpu;
+    proc->priority = v[i].priority;
+    memcpy(proc->nome,v[i].nome,sizeof(proc->nome));
+    pushQueue(queue,proc);
+    printf("%s %ld %ld\n", v[i].nome, v[i].burst_cpu, v[i].priority);
   }
 
   // First Come First Serve
   printf("\n\nFCFS ordine di esecuzione: ");
-  for (int i = 0; i < n; i++) {
-    printf("%s ", v[i].nome);
-    wait = wait + v[i].burst_cpu * (n - i - 1);
-  }
-  wait = wait / n;
-  printf("\nTempo medio di attesa: ");
-  printf("%f \n", wait);
-
-  wait = 0;
+  printf("\nTempo medio di attesa: %f \n", printBurst(v,n));
 
   // Shortest Job First
-  sort_SJF(v, n); // per ordinare in base al burst
 
+  //sort(v, n, compareBurst); // OLD BUBBLE
+
+  qsort(v, n, sizeof(proc_t), compareBurst);
   printf("SJF ordine di esecuzione: ");
-  for (int i = 0; i < n; i++) {
-    printf("%s ", v[i].nome);
-    wait = wait + v[i].burst_cpu * (n - i - 1);
-  }
-  wait = wait / n;
-  printf("\nTempo medio di attesa: ");
-  printf("%f \n", wait);
-
-  wait = 0;
+  printf("\nTempo medio di attesa: %f \n", printBurst(v,n));
 
   // Priority
-  sort_priority(v, n); // per ordinare in base alla priority
 
+  //sort(v, n, comparePriority); // OLD BUBBLE
+
+  qsort(v, n, sizeof(proc_t), comparePriority);
   printf("Priority ordine di esecuzione: ");
-  for (int i = 0; i < n; i++) {
-    printf("%s ", v[i].nome);
-    wait = wait + v[i].burst_cpu * (n - i - 1);
-  }
-  wait = wait / n;
-  printf("\nTempo medio di attesa: ");
-  printf("%f \n", wait);
+  printf("\nTempo medio di attesa: %f \n", printBurst(v,n));
 
   // Round Robin
   printf("Round Robin ordine di esecuzione: ");
 
-  for (int i = 0; i < n; i++) {
-    strcpy(v[i].nome, v_mod[i].nome);
-    v[i].burst_cpu = v_mod[i].burst_cpu;
-    v[i].priority = v_mod[i].priority;
-  }
-
-  int cont = 0, tempo = 0, flag = 0;
-  int cp[20];
-  int rimasti = n;
-
-  wait = 0;
-
-  // copia dei burst_cpu per modificarli
-  for (cont = 0; cont < n; cont++) {
-    cp[cont] = v[cont].burst_cpu;
-  }
-
-  cont = 0;
-
-  do {
-    if (cp[cont] <= quanto && cp[cont] > 0) {
-      tempo += cp[cont];
-      printf("%s[%d] ", v[cont].nome, cp[cont]);
-      cp[cont] = 0;
-      flag = 1;
-    } else if (cp[cont] > 0) {
-      cp[cont] -= quanto;
-      tempo += quanto;
-      printf("%s[%d] ", v[cont].nome, quanto);
+  unsigned tempo = 0;
+  float wait = 0;
+ 
+  for(;;){
+    proc_t* popped = popQueue(queue);
+    if (popped == NULL){
+      break;
     }
-
-    if (cp[cont] == 0 && flag == 1) {
-      rimasti--;
-      wait += tempo - v[cont].burst_cpu;
-      flag = 0;
+    if (popped->burst_cpu > 0){
+      printf("%s[%ld] ", popped->nome, popped->burst_cpu);
+      tempo += ((popped->burst_cpu > quanto) ? quanto : popped->burst_cpu);
+      popped->burst_cpu -= quanto;
+      pushQueue(queue,popped);
+      if (popped->burst_cpu <= 0){
+        wait += tempo - popped->perm_burst;
+        free(popped);
+      }
     }
+}
 
-    if (cont == n - 1)
-      cont = 0;
-    else if (tempo >= 0)
-      cont++;
-
-  } while (rimasti != 0);
-
-  printf("\nTempo medio di attesa: ");
-  printf("%f\n", wait / n);
-
-  return 0;
+printf("\nTempo medio di attesa: %f\n", wait/n);
+free(v);
+freeQueue(queue);
+return 0;
 }
